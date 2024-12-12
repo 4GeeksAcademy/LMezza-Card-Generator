5f1c3c548f8ab9a59a73c0a5b528fc8394af8daf
LMezza <mezabalza18@gmail.com>
2024-12-12 01:13:31 +0000

    Generador de carta aleatorio terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbols = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "1",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  let color = ["black", "red"];
+
+  newCard();
+  function newCard() {
+    let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+    let number = numbers[Math.floor(Math.random() * numbers.length)];
+    let ramdomColor = color[Math.floor(Math.random() * color.length)];
+    document.getElementById("top-card").innerHTML = symbol;
+    document.getElementById("top-card").style.color = ramdomColor;
+    document.getElementById("center-body").innerHTML = number;
+    document.getElementById("footer-card").innerHTML = symbol;
+    document.getElementById("footer-card").style.color = ramdomColor;
+  }
+  document.getElementById("boton").onclick = function() {
+    newCard();
+  };
 };
